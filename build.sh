0dd04676547e204d74f55eb22f482bf3273e4cd9
Ethan Joseph Kamus <ethanjpkamus@csu.fullerton.edu>
2019-10-10 10:18:50 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,13 @@ rm *.exe
 echo View the list of source files
 ls -l
 
-echo "Compile the file straight-line-functions.cs:"
-mcs -target:library -r:System.Windows.Forms -out:straight-functions.dll straight-line-functions.cs
+echo "Compile squarepathuserinterface.cs:"
+mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -out:squarepathuserinterface.dll squarepathuserinterface.cs
 
-echo "Compile straight-line-travel-user-interface.cs:"
-mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -r:straight-functions.dll -out:straight-line.dll straight-line-travel-user-interface.cs
-
-echo "Compile and link straight-line-travel-main.cs:"
-mcs -r:System -r:System.Windows.Forms -r:straight-line.dll -out:go.exe straight-line-travel-main.cs
+echo "Compile and link squarepathmain.cs:"
+mcs -r:System -r:System.Windows.Forms -r:straight-line.dll -out:squarepathmain.exe squarepathmain.cs
 
 echo "Run the program Straight Line Travel"
-./go.exe
+./squarepathmain.exe
 
 echo "The bash script has terminated."
